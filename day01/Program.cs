90d922459a6b4d485c3072dd0de04658d0292070
Rick James <rkjames@hotmail.com>
2024-12-01 14:22:55 -0800

    day 1 part 2 using linq as tmbarker did

--- day01/Program.cs
+++ day01/Program.cs
@@ -2,21 +2,22 @@
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
         int sum = 0;
-        //string[] lines = File.ReadAllLines("example.txt");
-        string[] lines = File.ReadAllLines("input.txt");
+        string[] lines = File.ReadAllLines("example.txt");
+        // string[] lines = File.ReadAllLines("input.txt");
         var left = new List<int>();
         var right = new List<int>();
 
         foreach (string line in lines)
         {
             var s = line.Split();
-            Console.WriteLine($"{s[0]} {s[3]}");
+            // Console.WriteLine($"{s[0]} {s[3]}");
             left.Add(int.Parse(s[0]));
             right.Add(int.Parse(s[3]));
         }
@@ -43,5 +44,8 @@ class Program
         }
 
         Console.WriteLine($"day 01p2 {sum}");
+
+        // LINQ: https://github.com/tmbarker/advent-of-code/blob/main/Solutions/Y2024/D01/Solution.cs
+        Console.WriteLine("day 01p2 linq: " + left.Sum(i => i * right.Count(c => c == i)));
     }
 }
