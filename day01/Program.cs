02f4b9e8b6a4b8f5dbc4e6cc977966db9254da46
Rick James <rkjames@hotmail.com>
2024-11-30 21:07:27 -0800

    day1 part 1

--- day01/Program.cs
+++ day01/Program.cs
@@ -1,4 +1,4 @@
-﻿// playing with advent 2023 day 1
+﻿// playing with advent 2024 day 1
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
@@ -8,23 +8,25 @@ class Program
     static void Main(string[] args)
     {
         int sum = 0;
+        //string[] lines = File.ReadAllLines("example.txt");
         string[] lines = File.ReadAllLines("input.txt");
+        var left = new List<int>();
+        var right = new List<int>();
 
         foreach (string line in lines)
         {
-            var digits = line.Where(char.IsDigit).Select(c => int.Parse(c.ToString())).ToList();
+            var s = line.Split();
+            Console.WriteLine($"{s[0]} {s[3]}");
+            left.Add(int.Parse(s[0]));
+            right.Add(int.Parse(s[3]));
+        }
 
-            if (digits.Count > 0)
-            {
-                int firstDigit = digits.First();
-                int lastDigit = digits.Last();
-                Console.WriteLine($"{firstDigit} {lastDigit}");
-                sum += firstDigit * 10 + lastDigit;
-            }
-            else
-            {
-                Console.WriteLine("No digits found in line.");
-            }
+        left.Sort();
+        right.Sort();
+        for (int i = 0; i < left.Count; i++)
+        {
+            int diff = Math.Abs(left[i] - right[i]);
+            sum += diff;
         }
 
         Console.WriteLine($"day 01 {sum}");
