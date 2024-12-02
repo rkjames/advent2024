e2f9c0f51352cd2241b87ecebdaead98f91ec9cc
Rick James <rkjames@hotmail.com>
2024-12-01 18:37:09 -0800

    day 2 skeleton

---
+++ day02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+//using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+
+// LINQ reminder
+// Console.WriteLine("day 01p2 linq: " + left.Sum(i => i * right.Count(c => c == i)));
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] lines = File.ReadAllLines("example.txt");
+        // string[] lines = File.ReadAllLines("input.txt");
+        var left = new List<int>();
+
+        foreach (string line in lines)
+        {
+            var s = line.Split();
+            Console.WriteLine($"{s[0]} {s[3]}");
+            left.Add(int.Parse(s[0]));
+        }
+
+    }
+}
