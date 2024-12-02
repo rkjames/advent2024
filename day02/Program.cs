ef19b1cf3aa6142a10f91fd4fa739055979e6931
Rick James <rkjames@hotmail.com>
2024-12-01 21:33:35 -0800

    day 2 part 2

--- day02/Program.cs
+++ day02/Program.cs
@@ -2,15 +2,59 @@
 //using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
 
 // LINQ reminders
 // Console.WriteLine("day 01p2 linq: " + left.Sum(i => i * right.Count(c => c == i)));
 // var digits = line.Where(char.IsDigit).Select(c => int.Parse(c.ToString())).ToList();
 class Program
 {
+    static bool check(List<int> list)
+    {
+        bool first = true;
+        int prev = 0;
+
+        bool positive = list.First() < list.Last();
+
+        foreach (var i in list)
+        {
+            if (first)
+            {
+                first = false;
+                prev = i;
+                continue;
+            }
+
+            // The levels are either all increasing or all decreasing.
+            if (positive && prev > i)
+            {
+                Console.WriteLine("positive: " + prev + " " + i);
+                return false;
+            }
+
+            if (!positive && prev < i)
+            {
+                Console.WriteLine("!positive: " + prev + " " + i);
+                return false;
+            }
+
+            // Any two adjacent levels differ by at least one and at most three.
+            var delta = Math.Abs(prev - i);
+            if (delta < 1 || delta > 3)
+            {
+                Console.WriteLine("delta: " + delta);
+                return false;
+            }
+
+            prev = i;
+        }
+
+        return true;
+    }
+
     static void Main(string[] args)
     {
-        //string[] lines = File.ReadAllLines("example.txt");
+        //string[] lines = File.ReadAllLines("example2.txt");
         string[] lines = File.ReadAllLines("input.txt");
         int safe_count = 0;
 
@@ -18,59 +62,39 @@ class Program
         {
             //Console.WriteLine(line);
             var s = line.Split();
-            bool first = true;
-            bool second = false;
-            bool positive = false;
-            bool safe = true;
-            int prev = 0;
+
+            var list = new List<int>();
+
             foreach (var p in s)
             {
                 var i = int.Parse(p);
-                if (first)
-                {
-                    first = false;
-                    second = true;
-                    prev = i;
-                    continue;                    
-                }
+                list.Add(i);
+            }
 
-                if (second)
-                {
-                    positive = prev < i;
-                    second = false;
-                }
+            var safe = check(list);
 
-                // The levels are either all increasing or all decreasing.
-                if (positive && prev > i)
+            if (!safe)
+            {
+                for (int i = 0; i < list.Count; i++)
                 {
-                    //Console.WriteLine("positive: " + prev + " " + i);
-                    safe = false;
-                    break;
+                    var clone = list.ToList();
+                    clone.RemoveAt(i);
+                    safe = check(clone);
+                    if (safe)
+                    {
+                        break;
+                    }
                 }
-
-                if (!positive && prev < i)
-                {
-                    //Console.WriteLine("!positive: " + prev + " " + i);
-                    safe = false;
-                    break;
-                }
-
-                // Any two adjacent levels differ by at least one and at most three.
-                var delta = Math.Abs(prev - i);
-                if (delta < 1 || delta > 3)
-                {
-                    //Console.WriteLine("delta: " + delta);
-                    safe = false;
-                    break;
-                }
-
-                prev = i;
             }
 
             if (safe)
             {
-                //Console.WriteLine("safe");
+                Console.WriteLine("safe: " + line);
                 safe_count++;
+            }
+            else
+            {
+                Console.WriteLine("unsafe: " + line);
             }
 
             //Console.WriteLine();
