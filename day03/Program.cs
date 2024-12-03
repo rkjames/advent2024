b61c3b4f7c93b0de6dc0938a0f5fcf41e0cc8ea4
Rick James <rkjames@hotmail.com>
2024-12-02 21:09:39 -0800

    day 3 part 1

--- day03/Program.cs
+++ day03/Program.cs
@@ -3,6 +3,7 @@
 using System.IO;
 using System.Linq;
 using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
 
 // LINQ reminders
 // Console.WriteLine("day 01p2 linq: " + left.Sum(i => i * right.Count(c => c == i)));
@@ -12,21 +13,25 @@ class Program
 
     static void Main(string[] args)
     {
-        string[] lines = File.ReadAllLines("example.txt");
-        // string[] lines = File.ReadAllLines("input.txt");
+        //string[] lines = File.ReadAllLines("example.txt");
+        string[] lines = File.ReadAllLines("input.txt");
+
+        var regex = new Regex(@"mul\((\d{1,3}),(\d{1,3})\)");
+        int sum = 0;
 
         foreach (string line in lines)
         {
-            //Console.WriteLine(line);
-            var s = line.Split();
-
-            foreach (var p in s)
+            //Console.WriteLine($"Line: {line}");
+            var matches = regex.Matches(line);
+            foreach (Match match in matches)
             {
-                var i = int.Parse(p);
+                //Console.WriteLine($"Match: {match.Value}");
+                //Console.WriteLine($"Group 1: {match.Groups[1].Value}");
+                //Console.WriteLine($"Group 2: {match.Groups[2].Value}");
+                sum += int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value);
             }
-
         }
 
-        Console.WriteLine("day 03p1: " );
+        Console.WriteLine($"day 03p1: {sum}" );
     }
 }
