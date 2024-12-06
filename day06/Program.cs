b14cd9ea5b983b3078840ec803b63a0e256a72d4
Rick James <rkjames@hotmail.com>
2024-12-05 21:19:59 -0800

    day 6 skeleton

---
+++ day06/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //string[] lines = File.ReadAllLines("example.txt");
+        string[] lines = File.ReadAllLines("input.txt");
+
+        int sum = 0;
+        var before = new Dictionary<int, List<int>>();
+        foreach (string line in lines)
+        {
+            if (line.Contains("|"))
+            {
+                var s = line.Split("|");
+                int key = int.Parse(s[0]);
+            }
+        }
+
+        Console.WriteLine($"day 06p1: {sum}");
+    }
+}
