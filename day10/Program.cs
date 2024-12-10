6422bfcffb7e49ddd2d80c45e77d94262a0de3de
Rick James <rkjames@hotmail.com>
2024-12-09 21:08:36 -0800

    day 10 skeleton

---
+++ day10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] lines = File.ReadAllLines("example.txt");
+        //string[] lines = File.ReadAllLines("input.txt");
+
+        int sum = 0;
+
+        for (int rowobs = 0; rowobs < lines.Length; rowobs++)
+        {
+        }
+
+        Console.WriteLine($"day 10p1: {sum}");
+    }
+}
