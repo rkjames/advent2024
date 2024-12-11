98773dc4de91d3100af41ffe7993372813406ac2
Rick James <rkjames@hotmail.com>
2024-12-10 21:49:01 -0800

    day 11 part 1, works for small numbers

--- day11/Program.cs
+++ day11/Program.cs
@@ -1,29 +1,88 @@
 ﻿using System;
+using System.ComponentModel;
 using System.IO;
+using System.Runtime.ExceptionServices;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 class Program
 {
+    // returns true if even split happened
+    static bool Split(int num, ref int first, ref int second)
+    {
+        first = 0;
+        second = 0;
+
+        // check if even
+        var temp = num;
+        int count = 0;
+        while (temp != 0)
+        {
+            temp /= 10;
+            count++;
+        }
+
+        if (count % 2 != 0)
+        {
+            return false;
+        }
+
+        int mask = (int)Math.Pow(10, count/2);
+        first = num / mask;
+        second = num % mask;
+
+        return true;
+    }
+
     static void Main(string[] args)
     {
-        string[] lines = File.ReadAllLines("example.txt");
-        //string[] lines = File.ReadAllLines("input.txt");
+        //string[] lines = File.ReadAllLines("example.txt");
+        string[] lines = File.ReadAllLines("input.txt");
 
-        int sum = 0;
-
-        for (int rowobs = 0; rowobs < lines.Length; rowobs++)
+        var list = new LinkedList<int>();
+        var s = lines[0].Split();
+        foreach (var chunk in s)
         {
-            for (int colobs = 0; colobs < lines[0].Length; colobs++)
+            list.AddLast(int.Parse(chunk));
+        }
+
+        for (int i = 0; i < 25; i++)
+        {
+            var cur = list.First;
+            while (cur != null)
             {
-                // strings are immutable.
-                char[][] grid = new char[lines.Length][];
-                for (int i = 0; i < lines.Length; i++)
+                if (cur.Value == 0)
                 {
-                    grid[i] = lines[i].ToCharArray();
+                    cur.Value = 1;
+                }
+                else
+                {
+                    int first = 0;
+                    int second = 0;
+                    if (Split(cur.Value, ref first, ref second))
+                    {
+                        list.AddBefore(cur, first);
+                        cur.Value = second;
+                    }
+                    else
+                    {
+                        var old = cur.Value;
+                        cur.Value *= 2024;
+                        if (cur.Value / 2024 != old)
+                        {
+                            throw new Exception($"overflow 2024: {old} {cur.Value}");
+                        }
+                    }
                 }
 
+                cur = cur.Next;
             }
         }
 
-        Console.WriteLine($"day 11p1: {sum}");
+        Console.WriteLine($"day 11p1: {list.Count}");
+        //foreach (var item in list)
+        //{
+        //    Console.Write(item + " ");
+        //}
+        //Console.WriteLine();
     }
 }
