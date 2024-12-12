2d9e61d912af46f8fec3e8a3fc90da928192386a
Rick James <rkjames@hotmail.com>
2024-12-11 21:13:28 -0800

    day 12 skeleton

---
+++ day12/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] lines = File.ReadAllLines("example.txt");
+        //string[] lines = File.ReadAllLines("input.txt");
+
+        var s = lines[0].Split();
+        Console.WriteLine($"day 12p1: 42");
+    }
+}
