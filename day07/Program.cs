c2cdcdf5a13ce9ba164421400266537e70e1d05e
Rick James <rkjames@hotmail.com>
2024-12-12 20:08:57 -0800

    day 7 skeleton

---
+++ day07/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] lines = File.ReadAllLines("example.txt");
+        //string[] lines = File.ReadAllLines("input.txt");
+
+        foreach (var line in lines)
+        {
+            Console.WriteLine(line);
+        }
+
+        int sum = 0;
+
+
+        Console.WriteLine($"day 07p1: {sum}");
+    }
+}
